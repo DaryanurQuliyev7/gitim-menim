63b017c69f90e830b47d8bd97e095edecf30d5ea
DaryanurQuliyev <deryanurquliyev@gmail.com>
2024-03-10 13:26:55 +0400

    yenisini

---
+++ pythontest/pythonum.py
@@ -0,0 +1,57 @@
+brithyear = input('enter brith year: ')
+age = 2023- int(brithyear)
+print('your age is',age)
+kilometr = input('How many meters is the length of the house and the workplace?: ')
+length = 1000  -int (kilometr)
+print('The distance between the work place and the place is equal to km ',length)
+
+
+brithYear = input('how old age you?: ')
+age = 2023 -int(brithYear)
+print('your age is',age)
+
+brithYear = input('enter brith year')
+age = 2023 - int(brithYear)
+print('your age is',age)
+
+
+brithYear = input('enter your brithdayyear :',)
+age = 2023 - int (brithYear)
+print('your age',age)
+
+brithYear = input('enter your brith year')
+age = 2023 - int(brithYear)
+print('sizin yasiniz',age)
+
+brithYear = input('enter brithday year')
+age= 2023 - int(brithYear)
+print('you age',age)
+
+brithYear = input('enter brith year')
+age = 2023 -int(brithYear)
+print('your age',age)
+
+
+
+
+brithYear = input('enter brith year')
+age = 2023 -int(brithYear)
+print('your age',age)
+
+
+
+
+brithdayYear = input('enter brithday year')
+age = 2023 -int(brithdayYear)
+print('your age',age)
+
+brithdayYear = input('enter brithday year')
+age = 2023 - int(brithdayYear)
+print('your age',age)
+
+
+
+distance = input('Distance (m)?')
+distanceInKilometers = int(distance)/1000
+print('your home,and to office distance of',distanceInKilometers,'km')
+
